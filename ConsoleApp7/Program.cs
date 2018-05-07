49635707a7114e06a97d7ba57e76d41a040557e2
Victoria <nozdracheva1997@mail.ru>
2018-05-07 17:47:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Massiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[3, 3];
+            int[,] b = new int[3, 3];
+            int[,] c = new int[3, 3];
+
+            Console.WriteLine();
+
+            Console.WriteLine("Введите первый массив!");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write("Массив [{0},{1}]= ", i,j);
+                    a[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Введите второй массив!");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write("Массив [{0},{1}]= ", i, j);
+                    b[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Равно: ");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    c[i, j] = a[i, j] + b[i, j]; 
+                    Console.Write("{0}\t", c[i,j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
